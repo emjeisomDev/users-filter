695a3cd4d539264448aa2bdb985dcd0cad542de4
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-01-02 15:20:58 -0300

    Passing the User List via @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    userSelected: IUser = {} as IUser;
-    showUserDetails: boolean = false;
+export class AppComponent  implements OnInit {
+
+  usersList: IUser[] = [];
+  userSelected: IUser = {} as IUser;
+  showUserDetails: boolean = false;
+
+  ngOnInit(){
+    setTimeout(()=>{
+      this.usersList = UsersList;
+    }, 2500);
+  }
 
   onUserSelected(user:IUser){
     this.showUserDetails = true;
